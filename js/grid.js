515591522f222513762efb4517cea1fa04ea1341
mac craig <maccraig98@gmail.com>
2017-06-15 19:13:35 -0700

    Show all photos, filter videos

--- js/grid.js
+++ js/grid.js
@@ -4,14 +4,16 @@ $(document).ready(function() {
     get: 'user',
     userId: '1545331378',
     //sortBy: 'most-liked',
-    accessToken: '1545331378.1677ed0.569d51abe4654b1485e73ea762c68498',
+    accessToken: '1545331378.44bf79b.c714b3c2e944473288f2368bb96aee3c',
     resolution: 'standard_resolution',
     limit: '100',
     template: '<div class="hidden-sm hidden-md hidden-xs col-xs-3"><a href="{{link}}"><img class="image" src="{{image}}"/></a></div><div class="hidden-sm hidden-lg hidden-xs col-xs-4"><a href="{{link}}"><img class="image" src="{{image}}"/></a></div><div class="hidden-lg hidden-md hidden-xs col-xs-6"><a href="{{link}}"><img class="image" src="{{image}}"/></a></div><div class="hidden-lg hidden-md hidden-sm col-xs-12"><a href="{{link}}"><img class="image" src="{{image}}"/></a></div>',
+    //filter: function(image) {
+    //  return image.tags.indexOf('cali') >= 0;
+    //}
     filter: function(image) {
       return image.type != 'video';
     }
-
   });
   feed.run();
 });
